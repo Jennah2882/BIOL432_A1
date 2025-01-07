91fc7e72b1fb752b9f1fa96a6db220106e313f56
Jennah2882 <jennahbean28@hotmail.com>
2025-01-07 12:02:41 -0500

    Update dataGenerato.R
    Added measuring units for limb width and length

--- dataGenerato.R
+++ dataGenerato.R
@@ -2,9 +2,9 @@
 species_names<- c("Oryctes nasicornis", "Dorcus parallelipipedus", "Protaetia orientalis", "Dynastes hercules", "Chalcosoma atlas")
 species_vector <- sample(species_names, size = 100, replace = TRUE)
 set.seed(52)
-#A vector of 100 values for Limb_width, randomly selected using a distribution of your choice that makes sense for the width of limbs.
+#A vector of 100 values for Limb_width (in mm), randomly selected using a distribution of your choice that makes sense for the width of limbs.
 limb_width <- runif(100, min=0, max=0.05)
-#A vector of 100 values for Limb_length, again randomly selected from a distribution of your choosing, but the numbers must make sense.
+#A vector of 100 values for Limb_length (in mm), again randomly selected from a distribution of your choosing, but the numbers must make sense.
 limb_length <- runif(100, min=0, max=80)
 #A vector of 100 values for Observer, randomly selected from a vector of 3 names
 observer_names<- c("Chappo", "Jennah", "Mohamed")
